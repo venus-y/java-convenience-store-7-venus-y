911e1d71f48df48d7a190d79bb195dd38f44aa47
venus01 <geumsung7769@naver.com>
2024-11-11 16:39:42 +0900

    Refactor : 영수증 객체 구조를 변경하였음
    영수증과 관련된 로직들을 StoreManager에서 Receipt 내부로 이동하였음

--- src/main/java/store/Receipt.java
+++ src/main/java/store/Receipt.java
@@ -13,19 +13,51 @@ public class Receipt {
     private final int finalPrice;
 
 
-    public Receipt(List<OrderItem> orderedItems, List<PromotionItem> promotionItems, int totalPrice, int totalQuantity, String memberShipChoice, int memberShipDiscountPrice, int promotionalDiscountPrice, int finalPrice) {
+    public Receipt(List<OrderItem> orderedItems, List<PromotionItem> promotionItems, String memberShipChoice) {
+
         this.orderedItems = orderedItems;
         this.promotionItems = promotionItems;
-        this.totalPrice = totalPrice;
-        this.totalQuantity = totalQuantity;
         this.isMembershipActive = updateMembershipChoice(memberShipChoice);
-        this.memberShipDiscountPrice = memberShipDiscountPrice;
-        this.promotionalDiscountPrice = promotionalDiscountPrice;
-        this.finalPrice = finalPrice;
+        this.totalPrice = orderedItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+        this.totalQuantity = orderedItems.stream()
+                .mapToInt(OrderItem::getQuantity)
+                .sum();
+        int totalPayWithoutPromotion = orderedItems.stream()
+                .mapToInt(OrderItem::getPayWithoutPromotionPrice)
+                .sum();
+        this.memberShipDiscountPrice = getMembershipDiscountPrice(memberShipChoice, totalPayWithoutPromotion);
+
+        this.promotionalDiscountPrice = promotionItems.stream()
+                .mapToInt(PromotionItem::promotionalDiscountPrice)
+                .sum();
+        this.finalPrice = totalPrice - promotionalDiscountPrice - memberShipDiscountPrice;
     }
 
     public int getTotalQuantity() {
         return totalQuantity;
+    }
+
+    public int getMembershipDiscountPrice(String membershipChoice, int totalPayWithoutPromotion) {
+        final String ACCEPTANCE_RESPONSE = "Y";
+        final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
+        final int NO_MEMBERSHIP_DISCOUNT = 0;
+
+        if (membershipChoice.equals(ACCEPTANCE_RESPONSE)) {
+            double membershipDiscountPrice = totalPayWithoutPromotion * MEMBERSHIP_DISCOUNT_RATE;
+            return getDiscountWithinLimit(membershipDiscountPrice);
+        }
+
+        return NO_MEMBERSHIP_DISCOUNT;
+    }
+
+    public int getDiscountWithinLimit(double membershipDiscountPrice) {
+        final int MAXIMUM_MEMBERSHIP_DISCOUNT = 8000;
+        if (membershipDiscountPrice >= MAXIMUM_MEMBERSHIP_DISCOUNT) {
+            return MAXIMUM_MEMBERSHIP_DISCOUNT;
+        }
+        return (int) membershipDiscountPrice;
     }
 
     public List<OrderItem> getOrderedItems() {
